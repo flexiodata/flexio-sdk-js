5033e98cc1fe9a6a0969d2df53fcc15f44266a36
David Z. Williams <dave@flex.io>
2017-09-08 16:48:47 -0400

    Release build (v1.5.2).

--- dist/flexio.js
+++ dist/flexio.js
@@ -1,5 +1,5 @@
 /*!
- * Flex.io Javascript SDK v1.5.1 (https://github.com/flexiodata/flexio-sdk-js)
+ * Flex.io Javascript SDK v1.5.2 (https://github.com/flexiodata/flexio-sdk-js)
  * (c) 2017 Gold Prairie LLC
  */
 (function webpackUniversalModuleDefinition(root, factory) {
@@ -6302,7 +6302,7 @@ var auth_token = '';
 
 exports.default = {
   version: function version() {
-    return "1.5.1";
+    return "1.5.2";
   },
   setup: function setup(token) {
     auth_token = token;
