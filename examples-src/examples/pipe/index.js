2e5aa15530a0d4b04bc8dfeacaa585149257f4f1
David Z. Williams <dave@flex.io>
2017-09-16 15:29:51 -0400

    Added another pipe example.

--- examples-src/examples/pipe/index.js
+++ examples-src/examples/pipe/index.js
@@ -1,9 +1,11 @@
 import pipe001 from './pipe-001'
 import pipe002 from './pipe-002'
 import pipe003 from './pipe-003'
+import pipe004 from './pipe-004'
 
 export default {
   pipe001,
   pipe002,
-  pipe003
+  pipe003,
+  pipe004
 }
--- examples-src/examples/pipe/pipe-002.js
+++ examples-src/examples/pipe/pipe-002.js
@@ -10,14 +10,19 @@ def flexio_handler(input, output):\\n\\
 
 Flexio.pipe()
   .execute(inline_python_code)
-  .email('flexio@mailinator.com', 'Flex.io JS SDK Test (inline Python) - ' + (new Date()).toString(), 'This is a test using inline Python code...', null, 'attachment')
+  .email(
+    'flexio@mailinator.com',
+    'Flex.io JS SDK Test (inline Python) - ' + (new Date()).toString(),
+    'This is a test using inline Python code...',
+    null,
+    'attachment'
+  )
   .run(function(err, process) {
     console.log(process)
   })`
 
 const fn = (Flexio, callback) => {
-
-var inline_python_code = "\
+  var inline_python_code = "\
 def flexio_handler(input, output):\n\
     writer = output.create(name='Hello')\n\
     if 'message' in input.env:\n\
@@ -25,10 +30,16 @@ def flexio_handler(input, output):\n\
     else:\n\
         writer.write('Hello, World!')"
 
-Flexio.pipe()
-  .execute(inline_python_code)
-  .email('flexio@mailinator.com', 'Flex.io JS SDK Test (inline Python) - ' + (new Date()).toString(), 'This is a test using inline Python code...', null, 'attachment')
-  .run(callback)
+  Flexio.pipe()
+    .execute(inline_python_code)
+    .email(
+      'flexio@mailinator.com',
+      'Flex.io JS SDK Test (inline Python) - ' + (new Date()).toString(),
+      'This is a test using inline Python code...',
+      null,
+      'attachment'
+    )
+    .run(callback)
 }
 
 export default {
