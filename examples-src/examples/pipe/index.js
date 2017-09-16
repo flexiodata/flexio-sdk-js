819fef4e2707866336c681ac21295654662bd754
David Z. Williams <dave@flex.io>
2017-09-16 15:35:11 -0400

    Added request examples.

--- examples-src/examples/pipe/index.js
+++ examples-src/examples/pipe/index.js
@@ -2,10 +2,16 @@ import pipe001 from './pipe-001'
 import pipe002 from './pipe-002'
 import pipe003 from './pipe-003'
 import pipe004 from './pipe-004'
+import pipe005 from './pipe-005'
+import pipe006 from './pipe-006'
+import pipe007 from './pipe-007'
 
 export default {
   pipe001,
   pipe002,
   pipe003,
-  pipe004
+  pipe004,
+  pipe005,
+  pipe006,
+  pipe007
 }
