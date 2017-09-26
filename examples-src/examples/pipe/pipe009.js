fe95aedec6db2579ccd76caf89c039f47b2683f1
David Z. Williams <dave@flex.io>
2017-09-26 12:03:24 -0400

    Added two new pipes.

---
+++ examples-src/examples/pipe/pipe009.js
@@ -0,0 +1,21 @@
+
+const code = `
+Flexio.pipe()
+  .input('https://raw.githubusercontent.com/flexiodata/data/master/mockaroo/names-and-ip-addresses.csv')
+  .convert('delimited', 'json')
+  .run(function(err, result) {
+    console.log(result)
+  })`
+
+const fn = (Flexio, callback) => {
+  Flexio.pipe()
+    .input('https://raw.githubusercontent.com/flexiodata/data/master/mockaroo/names-and-ip-addresses.csv')
+    .convert('delimited', 'json')
+    .run(callback)
+}
+
+export default {
+  title: 'CSV to JSON conversion in two lines of code',
+  code,
+  fn
+}
---
+++ examples-src/examples/pipe/pipe010.js
@@ -0,0 +1,21 @@
+
+const code = `
+Flexio.pipe()
+  .input('googledrive', 'flexio-google-drive', ['/test_data/data_payment.csv'])
+  .convert('delimited', 'json')
+  .run(function(err, result) {
+    console.log(result)
+  })`
+
+const fn = (Flexio, callback) => {
+  Flexio.pipe()
+    .input('googledrive', 'flexio-google-drive', ['/test_data/data_payment.csv'])
+    .convert('delimited', 'json')
+    .run(callback)
+}
+
+export default {
+  title: 'Read a CSV file from Google Drive and convert it to JSON',
+  code,
+  fn
+}
