98a02d0445fc1ae9b3f278c684fdfbfb4938a1b8
David Z. Williams <dave@flex.io>
2017-09-26 09:51:15 -0400

    Working on refining the examples for API-based use cases.

--- examples-src/App.vue
+++ examples-src/App.vue
@@ -1,44 +1,60 @@
 <template>
-  <div id="app" class="pa3 center" style="max-width: 1152px;">
-    <h1 class="mt0 pb2 bb b--black-10">Flex.io Javascript SDK v{{version}}</h1>
-
-    <h2>API Key</h2>
-
-    <p class="lh-copy">Flexio SDK calls require an API key. A default API key is provided to test the following calls. Feel free to <a href="https://www.flex.io/app/signup" target="_blank">sign up for Flex.io</a> to use your own API key in order to test these calls with your Flex.io account:</p class="lh-copy">
-    <form>
-      <div class="flex flex-row items-center mt2">
-        <label class="mr2">Use this API key:</label>
-        <input type="text" class="border-box ttl ba b--black-20 f6 pa2 w5 code" v-model="api_key">
+  <div id="app">
+    <div class="fixed top-0 w-100 z-1 flex-none flex flex-row items-center pv3 ph4 bg-blue white">
+      <div class="flex-fill">
+        <a :href="'https://github.com/flexiodata/flexio-sdk-js/releases/tag/v'+version" class="no-underline underline-hover" style="color: white">Flex.io Javascript SDK {{version}}</a>
       </div>
-    </form>
+      <div class="flex-none">
+        <a href="https://github.com/flexiodata/flexio-sdk-js" rel="noopener" target="_blank" title="View on Github"><svg xmlns="http://www.w3.org/2000/svg" fill="white" width="32" height="32" viewBox="0 0 16 16"><path d="M8 .198c-4.418 0-8 3.582-8 8 0 3.535 2.292 6.533 5.47 7.59.4.075.548-.173.548-.384 0-.19-.008-.82-.01-1.49-2.227.485-2.696-.943-2.696-.943-.364-.924-.888-1.17-.888-1.17-.726-.497.055-.486.055-.486.802.056 1.225.824 1.225.824.714 1.223 1.872.87 2.328.665.072-.517.28-.87.508-1.07-1.776-.202-3.644-.888-3.644-3.954 0-.874.313-1.588.824-2.148-.083-.202-.357-1.015.077-2.117 0 0 .672-.215 2.2.82.64-.177 1.323-.266 2.003-.27.68.004 1.365.093 2.004.27 1.527-1.035 2.198-.82 2.198-.82.435 1.102.162 1.916.08 2.117.512.56.822 1.274.822 2.147 0 3.072-1.872 3.748-3.653 3.946.288.248.544.735.544 1.48 0 1.07-.01 1.933-.01 2.196 0 .213.145.462.55.384 3.178-1.06 5.467-4.057 5.467-7.59 0-4.418-3.58-8-8-8z"></path></svg></a>
+      </div>
+    </div>
+    <div class="flex-fill flex flex-column flex-row items-stretch">
+      <div class="flex flex-row flex-column flex-none items-stretch br-l bb bb-0-l b--black-10 pv0 ph3-l overflow-auto trans-pm" style="padding-top: 6rem; padding-bottom: 6rem; min-width: 12rem; display: none">
+        Nav items go here...
+      </div>
 
-    <h2>Setup</h2>
+      <div class="flex-fill relative center ph4" style="padding-top: 6rem; padding-bottom: 6rem; max-width: 1152px">
+        <h1 class="mt0 pb2 bb b--black-10">Flex.io Javascript SDK v{{version}}</h1>
 
-    <example
-      title="Initialize Flex.io JS SDK with your API key"
-      code="Flexio.setup('YOUR API KEY GOES HERE')"
-      :show-run="false"
-    ></example>
+        <h2>API Key</h2>
 
-    <h2>Collections</h2>
+        <p class="lh-copy">Flexio SDK calls require an API key. A default API key is provided to test the following calls. Feel free to <a href="https://www.flex.io/app/signup" target="_blank">sign up for Flex.io</a> to use your own API key in order to test these calls with your Flex.io account:</p class="lh-copy">
+        <form>
+          <div class="flex flex-row items-center mt2">
+            <label class="mr2">Use this API key:</label>
+            <input type="text" class="border-box ttl ba b--black-20 f6 pa2 w5 code" v-model="api_key">
+          </div>
+        </form>
 
-    <example
-      v-for="(example, index) in list_examples"
-      :title="example.title"
-      :code="example.code"
-      :fn="example.fn"
-      :api-key="api_key"
-    ></example>
+        <h2>Setup</h2>
 
-    <h2>Pipes</h2>
+        <example
+          title="Initialize Flex.io JS SDK with your API key"
+          code="Flexio.setup('YOUR API KEY GOES HERE')"
+          :show-run="false"
+        ></example>
 
-    <example
-      v-for="(example, index) in pipe_examples"
-      :title="example.title"
-      :code="example.code"
-      :fn="example.fn"
-      :api-key="api_key"
-    ></example>
+        <h2>Collections</h2>
+
+        <example
+          v-for="(example, index) in list_examples"
+          :title="example.title"
+          :code="example.code"
+          :fn="example.fn"
+          :api-key="api_key"
+        ></example>
+
+        <h2>Pipes</h2>
+
+        <example
+          v-for="(example, index) in pipe_examples"
+          :title="example.title"
+          :code="example.code"
+          :fn="example.fn"
+          :api-key="api_key"
+        ></example>
+      </div>
+    </div>
   </div>
 </template>
 
