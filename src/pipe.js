31ce2f6f56b54f1013babc3abfc1ffe2efe33d0c
David Z. Williams <dave@flex.io>
2017-09-29 00:55:30 -0400

    Use `Flexio.task` functions for all pipe task functions.

--- src/pipe.js
+++ src/pipe.js
@@ -1,71 +1,6 @@
 import _ from 'lodash'
 import util from './util'
-import flexio from './flexio'
-
-import * as ttypes from './constants/task-type'
-import * as ctypes from './constants/connection-type'
-
-var toBase64 = function(str) {
-  try { return btoa(unescape(encodeURIComponent(str))) } catch(e) { return e }
-}
-
-var fromBase64 = function(str) {
-  try { return decodeURIComponent(escape(atob(str))) } catch(e) { return e }
-}
-
-var getJsFunctionBody = function(f) {
-  var body
-
-  try {
-    // stringify the function
-    body = f.toString()
-
-    // remove the wrapper function and just return its body
-    body = body.substring(body.indexOf('{') + 1, body.lastIndexOf('}'))
-  } catch (e) {
-    body = ''
-  }
-
-  return body
-}
-
-var getJsExport = function(f) {
-  if (_.isString(f))
-    return f
-
-  if (_.isFunction(f))
-  {
-    var body
-
-    if (f.length == 0)
-    {
-      // function has no parameters, so just take body
-      body = getJsFunctionBody(f)
-      return 'exports.flexio_file_handler = function(input, output) ' + body
-    }
-     else
-    {
-      body = f.toString()
-      if (body.substring(0, 8) == 'function')
-      {
-        // remove `function` but keep the provided arguments
-        body = body.slice(8)
-      }
-       else
-      {
-        // handle arrow syntax
-        var arrow = body.indexOf('=>')
-        var brace = body.indexOf('{')
-
-        if (arrow >= 0 && arrow < brace)
-        {
-          body = body.replace('=>\s*{', '{')
-        }
-      }
-      return 'exports.flexio_file_handler = function' + body
-    }
-  }
-}
+import Flexio from './flexio'
 
 export default (auth_token) => {
   return _.assign({}, {
@@ -122,7 +57,7 @@ export default (auth_token) => {
       this.loading = true
       util.debug.call(this, 'Loading Pipe `' + identifier + '`...')
 
-      flexio.http().get('/pipes/' + identifier)
+      Flexio.http().get('/pipes/' + identifier)
         .then(response => {
           var obj = _.get(response, 'data', {})
           this.pipe = _.assign({}, obj)
@@ -163,7 +98,7 @@ export default (auth_token) => {
       this.saving = true
       util.debug.call(this, 'Saving Pipe `' + _.get(this.pipe, 'name', 'Untitled Pipe') + '`...')
 
-      flexio.http().post('/pipes', this.pipe)
+      Flexio.http().post('/pipes', this.pipe)
         .then(response => {
           var pipe = _.get(response, 'data', {})
           this.pipe = _.assign({}, pipe)
@@ -209,14 +144,14 @@ export default (auth_token) => {
         process_mode: 'R'
       })
 
-      flexio.http().post('/processes', run_params)
+      Flexio.http().post('/processes', run_params)
         .then(response => {
           var obj = _.get(response, 'data', {})
           var process_eid = _.get(obj, 'eid', '')
           this.processes.push(obj)
           util.debug.call(this, 'Created Process.')
 
-          flexio.http().post('/processes/'+process_eid+'/run')
+          Flexio.http().post('/processes/'+process_eid+'/run')
             .then(response => {
               var obj2 = _.get(response, 'data', {})
               util.debug.call(this, 'Process Complete.')
@@ -237,317 +172,19 @@ export default (auth_token) => {
       return this
     },
 
-    // -- tasks --
+    // -- tasks (simply add the JSON object created by the split-out task functions) --
 
-    input() {
-      var type = ttypes.TASK_TYPE_INPUT
-      var args = Array.from(arguments)
-      var connection_type = _.get(args, '[0]', '')
-      var connection = undefined
-      var items = undefined
-
-      if (args.length == 0)
-        return util.debug.call(this, 'The input task requires at least 1 parameter')
-
-      switch (connection_type)
-      {
-        default:
-          connection_type = ctypes.CONNECTION_TYPE_HTTP
-          items = [].concat(args)
-          break
-
-        case ctypes.CONNECTION_TYPE_AMAZONS3:
-        case ctypes.CONNECTION_TYPE_DROPBOX:
-        case ctypes.CONNECTION_TYPE_ELASTICSEARCH:
-        case ctypes.CONNECTION_TYPE_GOOGLEDRIVE:
-        case ctypes.CONNECTION_TYPE_GOOGLESHEETS:
-        case ctypes.CONNECTION_TYPE_HTTP:
-        case ctypes.CONNECTION_TYPE_MYSQL:
-        case ctypes.CONNECTION_TYPE_POSTGRES:
-        case ctypes.CONNECTION_TYPE_SFTP:
-          connection = _.get(args, '[1]', '')
-          items = _.get(args, '[2]', [])
-          break
-
-        case ctypes.CONNECTION_TYPE_RSS:
-          connection = connection_type
-          items = _.get(args, '[1]', [])
-          break
-      }
-
-      items = _.map(items, (item) => {
-        return {
-          path: item
-        }
-      })
-
-      return this.addTask({
-        type,
-        metadata: { connection_type },
-        params: {
-          connection,
-          items
-        }
-      })
-    },
-
-    output() {
-      var type = ttypes.TASK_TYPE_OUTPUT
-      var args = Array.from(arguments)
-      var connection_type = _.get(args, '[0]', '')
-      var connection = undefined
-      var location = undefined
-
-      if (args.length == 0)
-        return util.debug.call(this, 'The output task requires at least 1 parameter')
-
-      switch (connection_type)
-      {
-        case ctypes.CONNECTION_TYPE_AMAZONS3:
-        case ctypes.CONNECTION_TYPE_ELASTICSEARCH:
-        case ctypes.CONNECTION_TYPE_GOOGLESHEETS:
-        case ctypes.CONNECTION_TYPE_MYSQL:
-        case ctypes.CONNECTION_TYPE_POSTGRES:
-          connection = _.get(args, '[1]', '')
-          break
-
-        case ctypes.CONNECTION_TYPE_DROPBOX:
-        case ctypes.CONNECTION_TYPE_GOOGLEDRIVE:
-        case ctypes.CONNECTION_TYPE_SFTP:
-          connection = _.get(args, '[1]', '')
-          location = _.get(args, '[2]', '/')
-          break
-      }
-
-      return this.addTask({
-        type,
-        metadata: { connection_type },
-        params: {
-          connection,
-          location
-        }
-      })
-    },
-
-    convert(input, output) {
-      var type = ttypes.TASK_TYPE_CONVERT
-
-      var task = {
-        type,
-        params: {}
-      }
-
-      var csv_format = {
-        format: 'delimited',
-        delimiter: '{comma}',
-        header: true,
-        qualifier: '{double-quote}'
-      }
-
-      var tsv_format = {
-        format: 'delimited',
-        delimiter: '{tab}',
-        header: true,
-        qualifier: '{none}'
-      }
-
-      // convert input
-
-      if (_.isString(input))
-      {
-        if (input == 'csv')
-          _.set(task, 'params.input', csv_format)
-           else if (input == 'tsv')
-          _.set(task, 'params.input', tsv_format)
-           else
-          _.set(task, 'params.input.format', input)
-      }
-       else if (_.isObject(input))
-      {
-        _.set(task, 'params.input', input)
-      }
-
-      // convert output
-
-      if (_.isString(output))
-      {
-        if (output == 'csv')
-          _.set(task, 'params.output', csv_format)
-           else if (output == 'tsv')
-          _.set(task, 'params.output', tsv_format)
-           else
-          _.set(task, 'params.output.format', output)
-      }
-       else if (_.isObject(output))
-      {
-        _.set(task, 'params.output', output)
-      }
-
-      return this.addTask(task)
-    },
-
-    email(to, subject, body_text, body_html, data) {
-      var type = ttypes.TASK_TYPE_EMAIL_SEND
-
-      if (_.isNil(to))
-        return util.debug.call(this, 'The `to` parameter is required')
-
-      if (_.isNil(subject))
-        return util.debug.call(this, 'The `subject` parameter is required')
-
-      if (_.isNil(body_text))
-        return util.debug.call(this, 'The `body_text` parameter is required')
-
-      // `to` parameter must be an array
-      if (!_.isArray(to))
-        to = [to]
-
-      if (_.isNil(body_html))
-        body_html = body_text
-
-      if (data != 'body' && data != 'attachment')
-        data = 'none'
-
-      return this.addTask({
-        type,
-        params: {
-          to,
-          subject,
-          body_text,
-          body_html,
-          data
-        }
-      })
-    },
-
-    execute() {
-      var type = ttypes.TASK_TYPE_EXECUTE
-      var args = Array.from(arguments)
-      var lang = _.get(args, '[0]', '')
-      var code = undefined
-
-      var task = {
-        type,
-        params: {}
-      }
-
-      // allow for flexible parameters
-      if (lang == 'python' || lang == 'javascript')
-      {
-        code = _.get(args, '[1]', '')
-      }
-       else
-      {
-        // default to javascript
-        lang = 'javascript'
-        code = _.get(args, '[0]', '')
-      }
-
-      if (_.isFunction(code))
-      {
-        // first argument is a function; we're using javascript
-        if (_.isNil(lang))
-          lang = 'javascript'
-
-        code = getJsFunctionBody(code)
-      }
-
-      // set the job's language
-      _.set(task, 'params.lang', lang)
-
-      // handle files or code snippets
-      var http_regex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
-      if (code.match(http_regex))
-        _.set(task, 'params.file', code)
-         else
-        _.set(task, 'params.code', toBase64(code))
-
-      return this.addTask(task)
-    },
-
-    filter(where) {
-      var type = ttypes.TASK_TYPE_FILTER
-
-      if (_.isNil(where))
-        return util.debug.call(this, 'The `filter` parameter is required')
-
-      return this.addTask({
-        type,
-        params: {
-          where
-        }
-      })
-    },
-
-    // shorthand for .execute('javascript', ...
-    javascript() {
-      var args = Array.from(arguments)
-      var code = _.get(args, '[0]', '')
-      code = getJsExport(code)
-      return this.execute('javascript', code)
-    },
-
-    limit(value) {
-      var type = ttypes.TASK_TYPE_LIMIT
-      value = _.defaultTo(value, 10)
-
-      return this.addTask({
-        type,
-        params: {
-          value
-        }
-      })
-    },
-
-    // shorthand for .execute('python', ...)
-    python() {
-      var args = Array.from(arguments)
-      return this.execute('python', _.get(args, '[0]', ''))
-    },
-
-    request() {
-      var type = ttypes.TASK_TYPE_REQUEST
-      var args = Array.from(arguments)
-      var params = _.get(args, '[0]', {})
-
-      // defaults
-      params = _.assign({
-        method: 'GET'
-      }, params)
-
-      return this.addTask({
-        type,
-        params
-      })
-    },
-
-    select() {
-      var type = ttypes.TASK_TYPE_SELECT
-      var columns = Array.from(arguments)
-
-      // handle the case where the user passed an array of items
-      // instead of just passing them as arguments
-      if (columns.length == 1 && _.isArray(_.get(columns, '[0]')))
-        columns = _.get(columns, '[0]', [])
-
-      return this.addTask({
-        type,
-        params: {
-          columns
-        }
-      })
-    },
-
-    sleep(value) {
-      var type = ttypes.TASK_TYPE_SLEEP
-      value = _.defaultTo(value, 10)
-
-      return this.addTask({
-        type,
-        params: {
-          value
-        }
-      })
-    }
+    input()      { return this.addTask(Flexio.task.input.apply(this, arguments))      },
+    output()     { return this.addTask(Flexio.task.output.apply(this, arguments))     },
+    convert()    { return this.addTask(Flexio.task.convert.apply(this, arguments))    },
+    email()      { return this.addTask(Flexio.task.email.apply(this, arguments))      },
+    execute()    { return this.addTask(Flexio.task.execute.apply(this, arguments))    },
+    filter()     { return this.addTask(Flexio.task.filter.apply(this, arguments))     },
+    javascript() { return this.addTask(Flexio.task.javascript.apply(this, arguments)) }, // shorthand for .execute('javascript', ...)
+    limit()      { return this.addTask(Flexio.task.limit.apply(this, arguments))      },
+    python()     { return this.addTask(Flexio.task.python.apply(this, arguments))     }, // shorthand for .execute('python', ...)
+    request()    { return this.addTask(Flexio.task.request.apply(this, arguments))    },
+    select()     { return this.addTask(Flexio.task.select.apply(this, arguments))     },
+    sleep()      { return this.addTask(Flexio.task.sleep.apply(this, arguments))      }
   })
 }
