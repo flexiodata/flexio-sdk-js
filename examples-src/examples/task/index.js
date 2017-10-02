3eb8290c0105b15cb5d79d7458a87140fa8678e8
David Z. Williams <dave@flex.io>
2017-10-02 10:25:25 -0400

    Split out task examples into separate file.

---
+++ examples-src/examples/task/index.js
@@ -0,0 +1,119 @@
+import Flexio from '../../../src/flexio'
+
+let tasks = []
+
+tasks.push({
+  title: 'Input (Web Link)',
+  description: "Flexio.task.input('https://raw.githubusercontent.com/flexiodata/data/master/mockaroo/names-and-ip-addresses.csv')",
+  obj: Flexio.task.input('https://raw.githubusercontent.com/flexiodata/data/master/mockaroo/names-and-ip-addresses.csv')
+})
+
+tasks.push({
+  title: 'Input (Google Drive)',
+  description: "Flexio.task.input('googledrive', 'flexio-google-drive', ['/test_data/data_payment.csv'])",
+  obj: Flexio.task.input('googledrive', 'flexio-google-drive', ['/test_data/data_payment.csv'])
+})
+
+tasks.push({
+  title: 'Output (Dropbox)',
+  description: "Flexio.task.output('dropbox', 'flexio-dropbox', '/test_data')",
+  obj: Flexio.task.output('dropbox', 'flexio-dropbox', '/test_data')
+})
+
+tasks.push({
+  title: 'Convert (best guess)',
+  description: 'Flexio.task.convert()',
+  obj: Flexio.task.convert()
+})
+
+tasks.push({
+  title: 'Convert from delimited to JSON',
+  description: "Flexio.task.convert('delimted', 'json')",
+  obj: Flexio.task.convert('delimted', 'json')
+})
+
+tasks.push({
+  title: 'Convert from CSV to JSON',
+  description: "Flexio.task.convert('csv', 'json')",
+  obj: Flexio.task.convert('csv', 'json')
+})
+
+tasks.push({
+  title: 'Convert from CSV to table',
+  description: "Flexio.task.convert('csv', 'table')",
+  obj: Flexio.task.convert('csv', 'table')
+})
+
+tasks.push({
+  title: 'Email',
+  description: "Flexio.task.email('fxtest101@mailinator', 'Subject of email', 'This is the body text')",
+  obj: Flexio.task.email('fxtest101@mailinator', 'Subject of email', 'This is the body text')
+})
+
+tasks.push({
+  title: 'Execute a remote python script',
+  description: "Flexio.task.execute('https://raw.githubusercontent.com/flexiodata/examples/master/functions/hello-world.py')",
+  obj: Flexio.task.execute('https://raw.githubusercontent.com/flexiodata/examples/master/functions/hello-world.py')
+})
+
+var description = `
+Flexio.task.javascript(function(input, output) {
+  output.write('Hello World!')
+})`
+
+tasks.push({
+  title: 'Execute Javascript (shorthand)',
+  description,
+  obj: Flexio.task.javascript(function(input, output) {
+    output.write('Hello World!')
+  })
+})
+
+tasks.push({
+  title: 'Filter',
+  description: "Flexio.task.filter(\"vend_no = '000042'\")",
+  obj: Flexio.task.filter("vend_no = '000042'")
+})
+
+tasks.push({
+  title: 'Limit',
+  description: "Flexio.task.limit(10)",
+  obj: Flexio.task.limit(10)
+})
+
+var description = `
+Flexio.task.request({
+  method: 'GET',
+  url: 'https://httpbin.org/get',
+  params: {
+    foo: 'bar',
+    munch: 'rank'
+  }
+})`
+
+tasks.push({
+  title: 'Request',
+  description,
+  obj: Flexio.task.request({
+    method: 'GET',
+    url: 'https://httpbin.org/get',
+    params: {
+      foo: 'bar',
+      munch: 'rank'
+    }
+  })
+})
+
+tasks.push({
+  title: 'Select',
+  description: "Flexio.task.select('vend_no', 'vend_name', 'amt_paid')",
+  obj: Flexio.task.select('vend_no', 'vend_name', 'amt_paid')
+})
+
+tasks.push({
+  title: 'Sleep',
+  description: "Flexio.task.sleep(5)",
+  obj: Flexio.task.sleep(5)
+})
+
+export default tasks
