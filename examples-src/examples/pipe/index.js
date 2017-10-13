90d90c08fdb3b32846c8775b802572dfac166e39
David Z. Williams <dave@flex.io>
2017-10-13 14:12:44 -0400

    Renaming example files.

--- examples-src/examples/pipe/index.js
+++ examples-src/examples/pipe/index.js
@@ -1,24 +1,22 @@
 
-import pipe001 from './pipe001'
-import pipe002 from './pipe002'
-import pipe003 from './pipe003'
 import pipe004 from './pipe004'
 import pipe009 from './pipe009'
 import pipe010 from './pipe010'
 import pipe011 from './pipe011'
 import execute_js_hello_world from './execute-js-hello-world'
 import execute_js_hello_world_2 from './execute-js-hello-world-2'
+import execute_python_hello_world_inline from './execute-python-hello-world-inline'
+import execute_python_hello_world_remote from './execute-python-hello-world-remote'
 import request from './request'
 
 export default {
   pipe009,
   pipe010,
   pipe011,
-  pipe001,
-  pipe002,
-  pipe003,
   pipe004,
   execute_js_hello_world,
   execute_js_hello_world_2,
+  execute_python_hello_world_inline,
+  execute_python_hello_world_remote,
   request
 }
