0d937f2547517aa88c8a93a0ca78bcf14aabe590
David Z. Williams <dave@flex.io>
2017-12-19 11:54:35 -0500

    Removed 'flexio.' prefix from task operation constants.

--- src/constants/task-op.js
+++ src/constants/task-op.js
@@ -1,73 +1,73 @@
 
 module.exports = {
-  TASK_OP_CALC: 'flexio.calc',
-  TASK_OP_COMMENT: 'flexio.comment',
-  TASK_OP_CONVERT: 'flexio.convert',
-  TASK_OP_COPY: 'flexio.copy',
-  TASK_OP_CREATE: 'flexio.create',
-  TASK_OP_CUSTOM: 'flexio.create',
-  TASK_OP_CUSTOM: 'flexio.custom',
-  TASK_OP_DISTINCT: 'flexio.distinct',
-  TASK_OP_DUPLICATE: 'flexio.duplicate',
-  TASK_OP_ECHO: 'flexio.echo',
-  TASK_OP_EMAIL_SEND: 'flexio.email',
-  TASK_OP_EXECUTE: 'flexio.execute',
-  TASK_OP_FIND_REPLACE: 'flexio.replace',
-  TASK_OP_FILTER: 'flexio.filter',
-  TASK_OP_GROUP: 'flexio.group',
-  TASK_OP_INPUT: 'flexio.input',
-  TASK_OP_INSERT: 'flexio.insert',
-  TASK_OP_LIMIT: 'flexio.limit',
-  TASK_OP_LIST: 'flexio.list',
-  TASK_OP_MERGE: 'flexio.merge',
-  TASK_OP_NOP: 'flexio.nop',
-  TASK_OP_OUTPUT: 'flexio.output',
-  TASK_OP_PROMPT: 'flexio.prompt',
-  TASK_OP_R: 'flexio.r',
-  TASK_OP_READ: 'flexio.read',
-  TASK_OP_RENAME: 'flexio.rename',
-  TASK_OP_RENDER: 'flexio.render',
-  TASK_OP_REQUEST: 'flexio.request',
-  TASK_OP_SEARCH: 'flexio.search',
-  TASK_OP_SELECT: 'flexio.select',
-  TASK_OP_SET: 'flexio.set',
-  TASK_OP_SLEEP: 'flexio.sleep',
-  TASK_OP_SORT: 'flexio.sort',
-  TASK_OP_TRANSFORM: 'flexio.transform',
-  TASK_OP_WRITE: 'flexio.write'
+  TASK_OP_CALC: 'calc',
+  TASK_OP_COMMENT: 'comment',
+  TASK_OP_CONVERT: 'convert',
+  TASK_OP_COPY: 'copy',
+  TASK_OP_CREATE: 'create',
+  TASK_OP_CUSTOM: 'create',
+  TASK_OP_CUSTOM: 'custom',
+  TASK_OP_DISTINCT: 'distinct',
+  TASK_OP_DUPLICATE: 'duplicate',
+  TASK_OP_ECHO: 'echo',
+  TASK_OP_EMAIL_SEND: 'email',
+  TASK_OP_EXECUTE: 'execute',
+  TASK_OP_FIND_REPLACE: 'replace',
+  TASK_OP_FILTER: 'filter',
+  TASK_OP_GROUP: 'group',
+  TASK_OP_INPUT: 'input',
+  TASK_OP_INSERT: 'insert',
+  TASK_OP_LIMIT: 'limit',
+  TASK_OP_LIST: 'list',
+  TASK_OP_MERGE: 'merge',
+  TASK_OP_NOP: 'nop',
+  TASK_OP_OUTPUT: 'output',
+  TASK_OP_PROMPT: 'prompt',
+  TASK_OP_R: 'r',
+  TASK_OP_READ: 'read',
+  TASK_OP_RENAME: 'rename',
+  TASK_OP_RENDER: 'render',
+  TASK_OP_REQUEST: 'request',
+  TASK_OP_SEARCH: 'search',
+  TASK_OP_SELECT: 'select',
+  TASK_OP_SET: 'set',
+  TASK_OP_SLEEP: 'sleep',
+  TASK_OP_SORT: 'sort',
+  TASK_OP_TRANSFORM: 'transform',
+  TASK_OP_WRITE: 'write'
 }
 
 /*
-export const TASK_OP_CALC          = 'flexio.calc'
-export const TASK_OP_COMMENT       = 'flexio.comment'
-export const TASK_OP_CONVERT       = 'flexio.convert'
-export const TASK_OP_COPY          = 'flexio.copy'
-export const TASK_OP_CREATE        = 'flexio.create'
-export const TASK_OP_CUSTOM        = 'flexio.custom'
-export const TASK_OP_DISTINCT      = 'flexio.distinct'
-export const TASK_OP_DUPLICATE     = 'flexio.duplicate'
-export const TASK_OP_ECHO          = 'flexio.echo'
-export const TASK_OP_EMAIL_SEND    = 'flexio.email'
-export const TASK_OP_EXECUTE       = 'flexio.execute'
-export const TASK_OP_FIND_REPLACE  = 'flexio.replace'
-export const TASK_OP_FILTER        = 'flexio.filter'
-export const TASK_OP_GROUP         = 'flexio.group'
-export const TASK_OP_INPUT         = 'flexio.input'
-export const TASK_OP_LIMIT         = 'flexio.limit'
-export const TASK_OP_LIST          = 'flexio.list'
-export const TASK_OP_MERGE         = 'flexio.merge'
-export const TASK_OP_NOP           = 'flexio.nop'
-export const TASK_OP_OUTPUT        = 'flexio.output'
-export const TASK_OP_PROMPT        = 'flexio.prompt'
-export const TASK_OP_R             = 'flexio.r'
-export const TASK_OP_READ          = 'flexio.read'
-export const TASK_OP_RENAME        = 'flexio.rename'
-export const TASK_OP_RENDER        = 'flexio.render'
-export const TASK_OP_REQUEST       = 'flexio.request'
-export const TASK_OP_SEARCH        = 'flexio.search'
-export const TASK_OP_SELECT        = 'flexio.select'
-export const TASK_OP_SLEEP         = 'flexio.sleep'
-export const TASK_OP_SORT          = 'flexio.sort'
-export const TASK_OP_TRANSFORM     = 'flexio.transform'
-export const TASK_OP_WRITE         = 'flexio.write'
+export const TASK_OP_CALC          = 'calc'
+export const TASK_OP_COMMENT       = 'comment'
+export const TASK_OP_CONVERT       = 'convert'
+export const TASK_OP_COPY          = 'copy'
+export const TASK_OP_CREATE        = 'create'
+export const TASK_OP_CUSTOM        = 'custom'
+export const TASK_OP_DISTINCT      = 'distinct'
+export const TASK_OP_DUPLICATE     = 'duplicate'
+export const TASK_OP_ECHO          = 'echo'
+export const TASK_OP_EMAIL_SEND    = 'email'
+export const TASK_OP_EXECUTE       = 'execute'
+export const TASK_OP_FIND_REPLACE  = 'replace'
+export const TASK_OP_FILTER        = 'filter'
+export const TASK_OP_GROUP         = 'group'
+export const TASK_OP_INPUT         = 'input'
+export const TASK_OP_LIMIT         = 'limit'
+export const TASK_OP_LIST          = 'list'
+export const TASK_OP_MERGE         = 'merge'
+export const TASK_OP_NOP           = 'nop'
+export const TASK_OP_OUTPUT        = 'output'
+export const TASK_OP_PROMPT        = 'prompt'
+export const TASK_OP_R             = 'r'
+export const TASK_OP_READ          = 'read'
+export const TASK_OP_RENAME        = 'rename'
+export const TASK_OP_RENDER        = 'render'
+export const TASK_OP_REQUEST       = 'request'
+export const TASK_OP_SEARCH        = 'search'
+export const TASK_OP_SELECT        = 'select'
+export const TASK_OP_SLEEP         = 'sleep'
+export const TASK_OP_SORT          = 'sort'
+export const TASK_OP_TRANSFORM     = 'transform'
+export const TASK_OP_WRITE         = 'write'
 */
