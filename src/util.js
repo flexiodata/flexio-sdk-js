a357fe43a19a8bfd47ce4faa74f3ac8a81a5297f
Ben Williams <ben@flex.io>
2018-01-04 17:26:03 +0100

    put util methods that are not dependent on the Flexio object on a higher scope

--- src/util.js
+++ src/util.js
@@ -1,11 +1,50 @@
 
-module.exports = {}
+util = {}
+module.exports = util
+
+
+util.isPipeObject = function(obj) {
+  return (typeof obj === 'object' && obj.hasOwnProperty('pipe') && typeof obj.pipe === 'object')
+}
+
+util.isPipeJson = function(obj) {
+  return (typeof obj === 'object' && obj.hasOwnProperty('op') && obj.hasOwnProperty('params'))
+}
+
+util.isNodeJs = function() {
+  return (Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]')
+}
+
+util.arrayBufferToString = function(buf) {
+
+  if (this.isNodeJs()) {
+    return buf.toString('utf-8')
+  } else {
+
+    if ("TextDecoder" in window) {
+      return (new TextDecoder('utf-8')).decode(buf)
+    } else {
+      var uint8arr = new Uint8Array(buf)
+      var utf8str = ''
+      var i, len = uint8arr.length
+      for (i = 0; i < len; ++i) {
+        utf8str += String.fromCharCode(uint8arr[i]);
+      }
+      return decodeURIComponent(escape(utf8str));
+    }
+  }
+}
+
 module.exports.getUtilObject = function(Flexio) {
 
   return new function() {
-    this.isNodeJs = function() {
-      return (Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]')
+
+    for (method in util) {
+      if (util.hasOwnProperty(method)) {
+        this[method] = util[method]
+      }
     }
+
 
     this.debug = function(msg) {
       var cfg = Flexio.getConfig()
@@ -18,24 +57,6 @@ module.exports.getUtilObject = function(Flexio) {
       return this
     }
 
-    this.arrayBufferToString = function(buf) {
 
-      if (this.isNodeJs()) {
-        return buf.toString('utf-8')
-      } else {
-
-        if ("TextDecoder" in window) {
-          return (new TextDecoder('utf-8')).decode(buf)
-        } else {
-          var uint8arr = new Uint8Array(buf)
-          var utf8str = ''
-          var i, len = uint8arr.length
-          for (i = 0; i < len; ++i) {
-            utf8str += String.fromCharCode(uint8arr[i]);
-          }
-          return decodeURIComponent(escape(utf8str));
-        }
-      }
-    }
   }
 }
