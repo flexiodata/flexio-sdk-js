cca324427d7d894e56173c4fadc541f6f4e741bd
Ben Williams <ben@flex.io>
2018-01-23 22:58:51 +0100

    fixed connections.list callback

--- src/connections.js
+++ src/connections.js
@@ -35,26 +35,17 @@ module.exports.getConnectionsObject = function(Flexio) {
       var args = Array.from(arguments)
       var callback = _.get(args, '[0]')
 
-      if (this.loading === true) {
-        setTimeout(() => { this.load.apply(this, arguments) }, 50)
-        return this
-      }
-
-      this.loading = true
       Flexio.util.debug('Requesting Connections...')
 
       Flexio.http().get('/connections')
         .then(response => {
           var items = _.get(response, 'data', [])
-          this.items = [].concat(items)
-          this.loading = false
           Flexio.util.debug('Success!')
 
           if (typeof callback == 'function')
             callback.call(this, null, items)
         })
         .catch(error => {
-          this.loading = false
           Flexio.util.debug('Failed.')
 
           if (typeof callback == 'function')
