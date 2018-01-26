8dcf8506c627bb3de26695dcc10a96dbecf6a6cb
Ben Williams <ben@flex.io>
2018-01-26 16:31:33 +0100

    correction to .request()

--- src/task/request.js
+++ src/task/request.js
@@ -2,9 +2,6 @@ var _ = require('lodash')                               // import _ from 'lodash
 var util = require('../util')                           // import util from '../util'
 var taskOps = require('../constants/task-op')           // import * as taskOps from '../constants/task-op'
 
-var method_types = ['GET', 'POST', 'DELETE', 'PUT', 'PATCH', 'HEAD', 'OPTIONS']
-var http_regex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
-
 /*
   syntax: request(url|connection[, config])
 
@@ -28,7 +25,6 @@ var request = function() {
   var args = Array.from(arguments)
   var url = _.get(args, '[0]', '')
   var params = _.get(args, '[1]', {})
-  var connection = undefined
 
   // if we specify all of the options as a param object, we're basically done
   // and we can skip all of the parameterization checks below
@@ -38,22 +34,7 @@ var request = function() {
   }
    else
   {
-    if (!_.isString(url))
-      throw 'The first parameter is required and must be a string representing the `url` or `connection` identifier.'
-
-    if (url.match(http_regex))
-    {
-      // first parameter is a URL; we're done
-    }
-     else
-    {
-      // first parameter is a connection identifier
-      connection = url
-      url = undefined
-    }
-
-    // default to `GET` method
-    params = _.assign({}, { url, connection }, params)
+    params = _.assign({}, { url }, params)
   }
 
   // default to `GET` method
