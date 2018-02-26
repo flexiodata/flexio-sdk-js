d8b375acdc1ad90947bd10614e28447520b1cf46
Ben Williams <ben@flex.io>
2018-02-26 13:42:08 +0100

    .foreach() supports for spec

--- src/task/foreach.js
+++ src/task/foreach.js
@@ -3,23 +3,36 @@ var util = require('../util')                           // import util from '../
 var taskOps = require('../constants/task-op')           // import * as taskOps from '../constants/task-op'
 
 // task definition function
-var foreach = function(run) {
+var foreach = function(p0, p1) {
 
-  if (util.isPipeObject(run))
-    run = run.pipe.task
-
-  return {
+  var res = {
     op: taskOps.TASK_OP_FOREACH,
-    params: {
-      run
-    }
+    params: {}
   }
+
+  if (typeof p0 === 'string' || p0 instanceof String) {
+    res.params.spec = p0
+  }
+
+  if (util.isPipeObject(p0)) {
+    res.params.run = p0.pipe.task
+  } else if (util.isPipeObject(p1)) {
+    res.params.run = p1.pipe.task
+  }
+
+  return res
 }
 
 foreach.toCode = function(json, Flexio) {
-  var params = _.get(json, 'params', {})
-  var msg = Flexio.task.toCode(params.run, Flexio)
-  return 'foreach(' + msg + ')'
+  var params = _.get(json, 'params', {}), p = []
+  if (params.hasOwnProperty('spec')) {
+    p.push(JSON.stringify(params.spec))
+  }
+  if (params.hasOwnProperty('run')) {
+    p.push(Flexio.task.toCode(params.run, Flexio))
+  }
+
+  return 'foreach(' + p.join(', ') + ')'
 }
 
 module.exports = foreach
