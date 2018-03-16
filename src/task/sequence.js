a882ac5243ef946aea72f485bfb384c03eff6f04
Ben Williams <ben@flex.io>
2018-03-16 16:20:24 +0100

    removed _.map dependency

--- src/task/sequence.js
+++ src/task/sequence.js
@@ -15,6 +15,33 @@ var sequence = function(steps) {
 
 sequence.toCode = function(json, Flexio) {
 
+  var retval = []
+
+  for (var i = 0; i < json.params.items.length; ++i) {
+    var task = json.params.items[i]
+
+    var cmd_str = Flexio.task.toCode(task, Flexio)
+
+    // TODO: review this; it makes the code that is output very nice and tidy,
+    //       however, if we don't like it we can yank it
+
+    // increase indent of all lines of the commands (except the Python execute task which cares about indents)
+    // other than the first line (which will be indented below)
+    if (task.hasOwnProperty('params') && task.params.hasOwnProperty('lang') && task.params.lang != 'python')
+      cmd_str = cmd_str.replace(/\n/g, '\n  ')
+
+    retval.push(cmd_str)
+  }
+
+  // prepend the start of the JS code
+  var retval = ['Flexio.pipe()'].concat(retval)
+
+  // indent tasks and add dot notation
+  return retval.join('\n  .')
+}
+
+
+/*
     // create JS task strings from JSON
     var retval = _.map(json.params.items, function(t) {
 
@@ -36,11 +63,7 @@ sequence.toCode = function(json, Flexio) {
   
     // indent tasks and add dot notation
     return retval.join('\n  .')
-  /*
-  var params = _.get(json, 'params', {})
-  var steps = JSON.stringify(params.steps) || '[]'
-  return 'sequence(' + steps + ')'
-  */
-}
+*/
+
 
 module.exports = sequence  // export default sequence
