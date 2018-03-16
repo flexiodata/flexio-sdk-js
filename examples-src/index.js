0dbaa94b527846276d52eda1f193629312d248fb
Ben Williams <ben@flex.io>
2018-03-16 13:24:37 +0100

    removed vue-highlightjs

--- examples-src/index.js
+++ examples-src/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
-import VueHighlightJS from 'vue-highlightjs'
 import App from './App.vue'
-
-// Tell Vue.js to use vue-highlightjs
-Vue.use(VueHighlightJS)
 
 const app = new Vue({
   el: '#app',
