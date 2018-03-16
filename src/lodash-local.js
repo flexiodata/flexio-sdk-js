b109e0c4c0a60906609b3782dae506e4d09ffdb7
Ben Williams <ben@flex.io>
2018-03-16 21:40:41 +0100

    replaced lodash assign with smaller version

--- src/lodash-local.js
+++ src/lodash-local.js
@@ -1,14 +1,36 @@
 
-module.exports = {
 
-  assign:        require('lodash.assign'),
+function getprop(object, path, defaultVal) {
+  var _path = Array.isArray(path) ? path : path.replace(/(\[(\d)\])/g, '.$2').split('.').filter(function(i) { return i.length })
+  if (!_path.length) {
+    return object === undefined ? defaultVal : object
+  }
+  return getprop(object[_path.shift()], _path, defaultVal)
+}
+
+function assign(target, options) {
+  var args = Array.from(arguments)
+  args.shift()
+  for (var i = 0; i < args.length; ++i) {
+    for (var prop in args[i]) {
+      if (args[i].hasOwnProperty(prop)) {
+        target[prop] = args[i][prop]
+      }
+    }
+  }
+  return target
+}
+
+
+module.exports = {
+  assign:        assign,
   isString:      function(value) { return typeof value === 'string' || value instanceof String },
   isPlainObject: require('lodash.isplainobject'),
   isNumber:      function(value) { return !isNaN(parseFloat(value)) && isFinite(value) },
-  get:           require('lodash.get'),
   isNil:         function(value) { return value == null },
   pick:          require('lodash.pick'),
   last:          require('lodash.last'),
   isFunction:    require('lodash.isfunction'),
-  defaultTo:     require('lodash.defaultto')
+  defaultTo:     require('lodash.defaultto'),
+  get:           getprop
 }
