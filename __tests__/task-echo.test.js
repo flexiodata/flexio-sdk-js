88c4e52d0c4fb5aaf6cc3c497ba42dd96263ce61
Ben Williams <ben@flex.io>
2018-03-17 21:07:31 +0100

    added more tests

--- __tests__/task-echo.test.js
+++ __tests__/task-echo.test.js
@@ -1,21 +1,13 @@
 var Flexio = require('../src/main.js')
 
 
-test('Flexio.task.echo; code to object', () => {
-  expect(
-    Flexio.task.echo('hello')
-  ).toEqual(
-    { op: 'echo', params: { msg: 'hello' } }
-  )
+test('Flexio.task.echo; code to object', async () => {
+
+  var pipe = Flexio.pipe().echo("Hello")
+
+  var response = await pipe.run()
+  var result = response.text
+
+  expect(result).toEqual("Hello")
 })
 
-
-test('Flexio.task.echo; object to code', () => {
-  var obj = { op: 'echo', params: { msg: 'hello' } }
-
-  expect(
-    Flexio.task.echo.toCode(obj)
-  ).toEqual(
-    `echo("hello")`
-  )
-})
--- src/lodash-local.js
+++ src/lodash-local.js
@@ -45,6 +45,6 @@ module.exports = {
   isNil:         function(value) { return value == null },
   defaultTo:     function(value, default_value) { return (value == null || value !== value) ? default_value : value },
   last:          function(array) { const length = array == null ? 0 : array.length; return length ? array[length - 1] : undefined; },
-  isFunction:    function(value) { return v instanceof Function; },
+  isFunction:    function(value) { return value instanceof Function; },
   isPlainObject: require('lodash.isplainobject')
 }
