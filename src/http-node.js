9a9a60205ee5fd5f0ed8d9ef62beeac1b9830df5
Ben Williams <ben@flex.io>
2018-03-22 19:03:31 +0100

    split out xhr and nodejs implementations

---
+++ src/http-node.js
@@ -0,0 +1,95 @@
+
+var _ = require('./lodash-local')
+
+function requestNodeJs(config) {
+
+    if (this.options.insecure === true) {
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+    }
+
+    var https = this.hasOwnProperty('https') ? this.https : null;
+    if (https === null) {
+        https = this.https = require('https')
+    }
+
+    var URL = require('url').URL
+    var url = new URL(_.get(config, 'url', ''), _.get(this.options, 'baseUrl', undefined))
+
+    var options = {
+        method: config.hasOwnProperty('method') ? config.method.toUpperCase() : 'GET',
+        port: (url.port ? url.port : 443),
+        host: url.hostname,
+        path: url.pathname,
+        encoding: null
+    }
+
+    var headers = _.assign({}, _.get(this.options, 'headers', {}), _.get(config, 'headers', {}))
+    if (Object.keys(headers).length > 0) {
+        options.headers = headers
+    }
+
+    var content_type = null
+    var postdata = _.get(config, 'data', null)
+    if (_.isPlainObject(postdata)) {
+        postdata = JSON.stringify(postdata)
+        content_type = 'application/json'
+    }
+    if (postdata) {
+        if (!options.hasOwnProperty('headers')) {
+            options.headers = {}
+        }
+        if (content_type) {
+            options.headers['Content-Type'] = content_type
+        }
+        options.headers['Content-Length'] = Buffer.byteLength(postdata)
+    }
+
+
+    return new Promise(function(resolve, reject) {
+
+        var resData = []
+
+        var req = https.request(options, function(res) {
+            
+            //console.log(`STATUS: ${res.statusCode}`)
+            //console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+            // res.setEncoding('binary')
+
+            var response = {
+                status: res.statusCode,
+                statusText: res.statusMessage,
+                headers: res.headers,
+                request: req
+            }
+
+            res.on('data', (chunk) => {
+                //console.log(`BODY: ${chunk}`)
+                resData.push(chunk)
+            })
+            res.on('end', () => {
+                // no more data in response
+                resData = Buffer.concat(resData)
+
+                if (config.responseType !== 'arraybuffer') {
+                    resData = resData.toString(config.responseEncoding);
+                }
+                //console.log("ONEND " + resData)
+                if (typeof resData === 'string') {
+                    try { resData = JSON.parse(resData) } catch(e) { }
+                }
+                response.data = resData
+                resolve(response)
+            })
+        })
+
+        req.on('error', (e) => {
+            reject(response)
+        })
+        
+        req.end(postdata)
+    }) // promise
+}
+
+
+
+module.exports = requestNodeJs
---
+++ src/http-xhr.js
@@ -0,0 +1,103 @@
+var _ = require('./lodash-local')
+
+function requestXHR(config) {
+
+    function parseResponseHeaders(headerstr) {
+        var headers = {};
+        var pairs = headerstr ? headerstr.split('\u000d\u000a') : []
+        for (var i = 0, len = pairs.length; i < len; i++) {
+            var comma = pairs[i].indexOf(':');
+            if (comma > 0) {
+                headers[pairs[i].substr(0, comma).trim()] = pairs[i].substr(comma+1).trim()
+            }
+        }
+        return headers
+    }
+
+
+    var headers = _.assign({}, _.get(this.options, 'headers', {}), _.get(config, 'headers', {}))
+    var postdata = _.get(config, 'data', null)
+
+    if ((typeof FormData !== 'undefined') && (postdata instanceof FormData)) {
+        delete headers['Content-Type'] // browser will take care of setting this
+    } else {
+        if (_.isPlainObject(postdata)) {
+            postdata = JSON.stringify(postdata)
+            headers['Content-Type'] = 'application/json'
+        }
+    }
+
+    return new Promise(function(resolve, reject) {
+
+        var xhr = new XMLHttpRequest()
+        xhr.open(config.method.toUpperCase(), config.url, true)
+        
+        if (config.responseType) {
+            xhr.responseType = config.responseType
+        }
+
+        if (Object.keys(headers).length > 0) {
+            for (var k in headers) {
+                if (headers.hasOwnProperty(k)) {
+                    xhr.setRequestHeader(k, headers[k])
+                }
+            }
+        }
+
+        function getResData(req) {
+            var resData = !config.responseType || config.responseType === 'text' ? req.responseText : req.response
+            if (typeof resData === 'string') {
+                try { resData = JSON.parse(resData) } catch(e) { }
+            }
+            return resData
+        }
+
+        xhr.onload = function () {
+
+            var resData = getResData(xhr)
+
+            var response = {
+                data: resData,
+                status: xhr.status === 1223 ? 204 : xhr.status, // IE sends 1223 instead of 204
+                statusText: xhr.status === 1223 ? 'No Content' : xhr.statusText,
+                headers: parseResponseHeaders(xhr.getAllResponseHeaders()),
+                config: config,
+                request: xhr
+            }
+
+            resolve(response)
+        
+            xhr = null
+        }
+
+        xhr.onerror = function handleError() {
+
+            var resData = getResData(xhr)
+
+            var response = {
+                data: resData,
+                status: xhr.status,
+                statusText: xhr.statusText,
+                headers: parseResponseHeaders(xhr.getAllResponseHeaders()),
+                config: config,
+                request: xhr
+            }
+
+            reject(response)
+
+            xhr = null
+        }
+
+        xhr.send(postdata)
+
+        // xhr.send('string');
+        // xhr.send(new Blob());
+        // xhr.send(new Int8Array());
+        // xhr.send({ form: 'data' });
+        // xhr.send(document);
+
+    }) // promise
+}
+
+
+module.exports = requestXHR
