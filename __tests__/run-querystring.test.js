5166da7e20b1c6ddf5bce0d9381c25841428d47e
Ben Williams <ben@flex.io>
2018-03-26 23:04:10 +0200

    added test for posting JSON data

--- __tests__/run-querystring.test.js
+++ __tests__/run-querystring.test.js
@@ -1,13 +1,13 @@
 var Flexio = require('../sdk-test-config')
 
-test('Flexio.task.echo; test query string', async () => {
+test('Flexio.task.echo; test posting JSON data', async () => {
 
   var pipe = Flexio.pipe()
                    .javascript(function(context) {
-                      context.output.write('Hello ' + context.query.name + '!')
+                      context.output.write('Hello ' + context.form.name + '!')
                    })
 
-  var response = await pipe.run({ query: { name: 'World' } })
+  var response = await pipe.run({ data: { name: 'World' } })
   var result = response.text
 
   expect(result).toEqual("Hello World!")
