fc0d4f1edfe692297e05ccff4a7a724650dcd7ed
Ben Williams <ben@flex.io>
2018-04-20 00:54:03 +0200

    added test for set task

---
+++ __tests__/task-set.test.js
@@ -0,0 +1,21 @@
+var Flexio = require('../sdk-test-config')
+
+
+test('Flexio.task.dump; code to object', () => {
+  expect(
+    Flexio.task.set('key','value')
+  ).toEqual(
+    {"op": "set", "params": {"var": "key", "value": "value"}}
+  )
+})
+
+
+test('Flexio.task.dump; object to code', () => {
+  var obj = {"op": "set", "params": {"var": "key", "value": "value"}}
+
+  expect(
+    Flexio.task.set.toCode(obj)
+  ).toEqual(
+    `set("key", "value")`
+  )
+})
