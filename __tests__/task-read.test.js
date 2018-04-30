8511517273930562b517f31948be3e9def018b2b
Ben Williams <ben@flex.io>
2018-05-01 00:23:30 +0200

    added test

---
+++ __tests__/task-read.test.js
@@ -0,0 +1,21 @@
+var Flexio = require('../sdk-test-config')
+
+
+test('Flexio.task.read; code to object', () => {
+  expect(
+    Flexio.task.read('/connection/file.txt')
+  ).toEqual(
+    { op: 'read', params: { path: '/connection/file.txt' } }
+  )
+})
+
+
+test('Flexio.task.read; object to code', () => {
+  var obj = { op: 'read', params: { path: '/connection/file.txt' } }
+
+  expect(
+    Flexio.task.read.toCode(obj)
+  ).toEqual(
+    `read("/connection/file.txt")`
+  )
+})
