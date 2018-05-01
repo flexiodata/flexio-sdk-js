2f905a02c0421b388cb7ba5cdf8c8509a86372c1
Ben Williams <ben@flex.io>
2018-05-01 23:15:45 +0200

    added copy task test

---
+++ __tests__/task-copy.test.js
@@ -0,0 +1,41 @@
+var Flexio = require('../sdk-test-config')
+
+
+test('Flexio.task.copy; code to object', () => {
+  expect(
+    Flexio.task.copy('a.txt', '/dest')
+  ).toEqual(
+    { op: 'copy', params: { from: 'a.txt', to: '/dest' } }
+  )
+})
+
+
+test('Flexio.task.copy; object to code', () => {
+  var obj = { op: 'copy', params: { from: 'a.txt', to: '/dest' } }
+
+  expect(
+    Flexio.task.copy.toCode(obj)
+  ).toEqual(
+    `copy("a.txt", "/dest")`
+  )
+})
+
+
+test('Flexio.task.copy; code to object with array', () => {
+  expect(
+    Flexio.task.copy(['a.txt','b.txt'], '/dest')
+  ).toEqual(
+    { op: 'copy', params: { from: ['a.txt','b.txt'], to: '/dest' } }
+  )
+})
+
+
+test('Flexio.task.copy; object to code with array', () => {
+  var obj = { op: 'copy', params: { from: ['a.txt','b.txt'], to: '/dest' } }
+
+  expect(
+    Flexio.task.copy.toCode(obj)
+  ).toEqual(
+    `copy(["a.txt","b.txt"], "/dest")`
+  )
+})
