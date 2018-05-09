0faee06dec92dbf0ac12205156addde2f791c286
Ben Williams <ben@flex.io>
2018-05-09 18:53:59 +0200

    added task write test

---
+++ __tests__/task-write.test.js
@@ -0,0 +1,31 @@
+var Flexio = require('../sdk-test-config')
+
+
+test('Flexio.task.write; code to object', () => {
+  expect(
+    Flexio.task.write('/connection/file.txt')
+  ).toEqual(
+    { op: 'write', path: '/connection/file.txt' }
+  )
+})
+
+
+test('Flexio.task.write; object to code', () => {
+  var obj = { op: 'write', path: '/connection/file.txt' }
+
+  expect(
+    Flexio.task.write.toCode(obj)
+  ).toEqual(
+    `write("/connection/file.txt")`
+  )
+})
+
+test('Flexio.task.write; object to code (with params subnode)', () => {
+  var obj = { op: 'write', params: { path: '/connection/file.txt' } }
+
+  expect(
+    Flexio.task.write.toCode(obj)
+  ).toEqual(
+    `write("/connection/file.txt")`
+  )
+})
